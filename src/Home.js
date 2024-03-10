e7704f66ed054d72967b90c92d2988df6db8db79
shaik sumaiya <132834754+sksumaiya191@users.noreply.github.com>
2024-03-10 17:17:13 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
               
             </div>
             <div className='fiicons-container'>
-              <a className='fisocial-icons' href='https://www.instagram.com/_smiley_sumaiya___/' target='_blank' rel="noreferrer">
+              <a className='fisocial-icons' href='https://www.instagram.com/smiley_sumaiya/' target='_blank' rel="noreferrer">
                 <FaInstagram />
               </a>
               <a className='fisocial-icons' href='https://github.com/sksumaiya191' target='_blank' rel="noreferrer">
